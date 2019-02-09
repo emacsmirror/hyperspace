5fdd680dc2e7b8a064cfdf93d6948546ff51afc2
Ian Eure <ian@retrospec.tv>
2019-02-08 22:44:32 -0800

    Cut v0.8.4

--- hyperspace.el
+++ hyperspace.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ian Eure <ian@retrospec.tv>
 ;; URL: https://github.com/ieure/hyperspace-el
-;; Version: 0.8.3
+;; Version: 0.8.4
 ;; Package-Requires: ((emacs "25") (s "1.12.0"))
 ;; Keywords: tools, convenience
 
